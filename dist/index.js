5095dfdeed283b2f203383fb73a187cf857b4890
Paul Kirkaas <pkirkaas@gmail.com>
2023-03-21 03:49:30 +0100

    Fixed pre-commit hook (I hope)

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+/**
+ * Main entry/export for pk-ts-sqlite-lib
+ */
+export * from './init.js';
+//# sourceMappingURL=index.js.map
---
+++ dist/init.js
@@ -0,0 +1,6 @@
+/**
+ * Initializations for Pk-Ts-Sqlite-Lib
+ */
+export * from 'pk-ts-node-lib';
+export * from './db-lib.js';
+//# sourceMappingURL=init.js.map
