e29fa0f719d6d9ad04f18d31b052a0f3ca2ebb5b
Paul Kirkaas <pkirkaas@gmail.com>
2023-10-24 23:23:40 +0200

    Hmm - export problems. Change tsconfig targets?

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -2,4 +2,6 @@
  * Main entry/export for pk-ts-sqlite-lib
  */
 export * from './init.js';
+export * from 'pk-ts-node-lib';
+export * from './db-lib.js';
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@
  * Main entry/export for pk-ts-sqlite-lib
  */
 export * from './init.js';
+export * from   'pk-ts-node-lib';
+export * from './db-lib.js';
